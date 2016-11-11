4036b2b05b19827e5d45d7d7774a39cb0f1543df
Noah Birnel <nbirnel@gmail.com>
2016-11-11 11:08:07 -0800

    add some python graph dicts

---
+++ directed-graph.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+#http://legacy.python.org/doc/essays/graphs/
+# Copyright (c) 1998, 2000, 2003 Python Software Foundation.
+# All rights reserved.
+# Licensed under the PSF license.
+
+# Directed graph code
+
+# Modified by Noah Birnel for python3 (replace 'has_key' with 'in')
+
+graph = {'A': ['B', 'C'],
+    'B': ['C', 'D'],
+    'C': ['D'],
+    'D': ['C'],
+    'E': ['F'],
+    'F': ['C']}
+
+
+def find_path(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    if not start in graph:
+        return None
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_path(graph, node, end, path)
+            if newpath: return newpath
+    return None
+
+def find_all_paths(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return [path]
+    if not start in graph:
+        return []
+    paths = []
+    for node in graph[start]:
+        if node not in path:
+            newpaths = find_all_paths(graph, node, end, path)
+            for newpath in newpaths:
+                paths.append(newpath)
+    return paths
+
+def find_shortest_path(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    if not start in graph:
+        return None
+    shortest = None
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_shortest_path(graph, node, end, path)
+            if newpath:
+                if not shortest or len(newpath) < len(shortest):
+                    shortest = newpath
+    return shortest
